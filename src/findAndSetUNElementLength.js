4f62c3abf9b3166e8667e17fc9400c37f080a7c2
sirrrich <richter_rolf@web.de>
2017-09-06 16:40:50 +0200

    Bugfix for buffer overrun with nested sequences
    If elements with UN contain nested sequences the calculation of the element length has to be done recursive.
    Maybe this solves the bug https://github.com/chafey/dicomParser/issues/62, but i have no images for testing.

--- src/findAndSetUNElementLength.js
+++ src/findAndSetUNElementLength.js
@@ -17,26 +17,26 @@ export default function findAndSetUNElementLength (byteStream, element) {
   const itemDelimitationItemLength = 8;
   const maxPosition = byteStream.byteArray.length - itemDelimitationItemLength;
 
-  while (byteStream.position <= maxPosition) {
-    const groupNumber = byteStream.readUint16();
-
-    if (groupNumber === 0xfffe) {
-      const elementNumber = byteStream.readUint16();
-
-      if (elementNumber === 0xe0dd) {
-        // NOTE: It would be better to also check for the length to be 0 as part of the check above
-        // but we will just log a warning for now
-        const itemDelimiterLength = byteStream.readUint32();
-
-        if (itemDelimiterLength !== 0) {
-          byteStream.warnings(`encountered non zero length following item delimiter at position ${byteStream.position - 4} while reading element of undefined length with tag ${element.tag}`);
+  while (byteStream.position <= maxPosition)
+	{
+		var tag = dicomParser.readTag(byteStream);
+		var length = byteStream.readUint32();
+		// parse nested sequences but ignore item tag
+		if(tag !== 'xfffee000' && length === 4294967295)
+        {
+            dicomParser.findAndSetUNElementLength(byteStream, element);
         }
-        element.length = byteStream.position - element.dataOffset;
-
-        return;
-      }
-    }
-  }
+		if (tag === 'xfffee0dd')
+		{
+			// NOTE: It would be better to also check for the length to be 0 as part of the check above
+			// but we will just log a warning for now
+			if (length !== 0) {
+				byteStream.warnings('encountered non zero length following item delimiter at position' + byteStream.position - 4 + " while reading element of undefined length with tag ' + element.tag");
+			}
+			element.length = byteStream.position - element.dataOffset;
+			return;
+		}
+	}
 
   // No item delimitation item - silently set the length to the end
   // of the buffer and set the position past the end of the buffer
